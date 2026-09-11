b1f98c08ef10a314a3cc1334a1b0c0387e2c8290
valeria.montano <montanovaleria55@gmail.com>
2026-09-10 19:00:51 -0600

    agregar: item.xml para una nuevas vista y tambien Destination modelo que guardaremos en FireBase

---
+++ app/src/main/java/com/example/desafio2_dsm/Destination.kt
@@ -0,0 +1,10 @@
+package com.example.desafio2_dsm
+
+data class Destination(
+    var id: String = "",
+    var name: String = "",
+    var country: String = "",
+    var price: Double = 0.0,
+    var description: String = "",
+    var imageUrl: String = ""
+)
